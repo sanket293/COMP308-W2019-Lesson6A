2dd8e3abd0841a4038e114d946f83d35493d573f
Sanket <scvagadiya@gmail.com>
2019-02-16 09:02:38 -0500

    initial commit

---
+++ server/routes/index.js
@@ -0,0 +1,22 @@
+let express = require('express');
+let router = express.Router();
+
+let indexController = require('../controllers/index');
+
+/* GET home page. */
+router.get('/', indexController.displayHomePage);
+
+/* GET about page. */
+router.get('/about', indexController.displayAboutPage);
+
+/* GET products page. */
+router.get('/products', indexController.displayProductsPage);
+
+/* GET services page. */
+router.get('/services', indexController.displayServicesPage);
+
+/* GET contact page. */
+router.get('/contact', indexController.displayContactPage);
+
+
+module.exports = router;
